c222d79e8bbcb5b0ea521dcf553bff0dc6cee4da
NikStan27 <45175980+NikStan27@users.noreply.github.com>
2018-12-08 14:48:31 +0100

    Update form.js

--- form.js
+++ form.js
@@ -3,23 +3,28 @@ const ime = document.getElementById('ime')
 const email = document.getElementById('email')
 const lozinka = document.getElementById('lozinka')
 
+let sveJeValidno = true
+
 function proveriIme() {
     if (ime.value.length > 3 && ime.value.length < 30 && ime.value.includes(' ')) {
-
+        
         ime.classList.add('valid')
         ime.classList.remove('invalid')
     }
     else {
+        sveJeValidno = false
         ime.classList.add('invalid')
         ime.classList.remove('valid')
     }
 }
 function proveriEmail() {
     if (email.value.includes('@') && email.value.includes('.')) {
+        
         email.classList.add('valid')
         email.classList.remove('invalid')
     }
     else {
+        sveJeValidno = false
         email.classList.add('invalid')
         email.classList.remove('valid')
 
@@ -30,10 +35,12 @@ function proveriEmail() {
 }
 function proveriLozinku() {
     if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(lozinka.value) ){
-    lozinka.classList.add('valid')
+       
+        lozinka.classList.add('valid')
     lozinka.classList.remove('invalid')
 
 } else {
+    sveJeValidno = false
     lozinka.classList.add('invalid')
     lozinka.classList.remove('valid')
 
@@ -42,9 +49,11 @@ function proveriLozinku() {
 
 function proveri(e) {
     e.preventDefault()
+  sveJeValidno = true
     proveriIme()
     proveriEmail()
     proveriLozinku()
+if (sveJeValidno) forma.submit()
 }
 
 
